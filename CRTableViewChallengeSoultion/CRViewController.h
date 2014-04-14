99c02219f2e018509eef9b92092ee6744c2f7876
Cooper Richardson <richardson.cooper@gmail.com>
2014-04-13 17:15:37 -0700

    Initial Commit

---
+++ CRTableViewChallengeSoultion/CRViewController.h
@@ -0,0 +1,13 @@
+//
+//  CRViewController.h
+//  CRTableViewChallengeSoultion
+//
+//  Created by Cooper Richardson on 4/13/14.
+//  Copyright (c) 2014 Cooper RIchardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CRViewController : UIViewController
+
+@end
---
+++ CRTableViewChallengeSoultion/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CRTableViewChallengeSoultion
+//
+//  Created by Cooper Richardson on 4/13/14.
+//  Copyright (c) 2014 Cooper RIchardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CRAppDelegate class]));
+    }
+}
